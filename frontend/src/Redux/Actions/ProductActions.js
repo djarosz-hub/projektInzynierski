65723dfef2867c15cac23d3f463c17d5de228935
djarosz-hub <jaroszdamian9@gmail.com>
2022-10-26 22:27:27 +0200

    adding redux product details

--- frontend/src/Redux/Actions/ProductActions.js
+++ frontend/src/Redux/Actions/ProductActions.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from './../Constants/ProductConstants';
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL } from './../Constants/ProductConstants';
 import axios from "axios";
 
 export const listProduct = () => async (dispatch) => {
@@ -12,6 +12,22 @@ export const listProduct = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+        const { data } = await axios.get(`/api/products/${id}`);
+
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
