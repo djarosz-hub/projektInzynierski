2c6836f636364c821914170a0480b22240de025e
djarosz-hub <jaroszdamian9@gmail.com>
2022-11-27 20:07:20 +0100

    shipping

--- frontend/src/Redux/Actions/CartActions.js
+++ frontend/src/Redux/Actions/CartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../Constants/CartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../Constants/CartConstants";
 import axios from "axios";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -23,4 +23,12 @@ export const removeFromCart = (id) => async (dispatch, getState) => {
         payload: id,
     });
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => async (dispatch) => {
+    dispatch({
+        type: CART_SAVE_SHIPPING_ADDRESS,
+        payload: data,
+    });
+    localStorage.setItem("shippingAddress", JSON.stringify(data));
 };
