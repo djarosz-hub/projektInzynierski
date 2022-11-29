d850a11217ef38c7f0455e3a6bf1c6be92ca7e67
djarosz-hub <jaroszdamian9@gmail.com>
2022-11-29 20:51:35 +0100

    place order

--- frontend/src/Redux/Actions/CartActions.js
+++ frontend/src/Redux/Actions/CartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../Constants/CartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../Constants/CartConstants";
 import axios from "axios";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -25,10 +25,18 @@ export const removeFromCart = (id) => async (dispatch, getState) => {
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
 
-export const saveShippingAddress = (data) => async (dispatch) => {
+export const saveShippingAddress = (data) => (dispatch) => {
     dispatch({
         type: CART_SAVE_SHIPPING_ADDRESS,
         payload: data,
     });
     localStorage.setItem("shippingAddress", JSON.stringify(data));
+};
+
+export const savePaymentMethod = (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_PAYMENT_METHOD,
+        payload: data,
+    });
+    localStorage.setItem("paymentMethod", JSON.stringify(data));
 };
--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,38 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import Header from "./../components/Header";
+import { useDispatch, useSelector } from 'react-redux';
+import { savePaymentMethod } from "../Redux/Actions/CartActions";
 
-const PaymentScreen = () => {
-  window.scrollTo(0, 0);
+const PaymentScreen = ({ history }) => {
+    window.scrollTo(0, 0);
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <>
-      <Header />
-      <div className="container d-flex justify-content-center align-items-center login-center">
-        <form
-          className="Login2 col-md-8 col-lg-4 col-11"
-          onSubmit={submitHandler}
-        >
-          <h6>SELECT PAYMENT METHOD</h6>
-          <div className="payment-container">
-            <div className="radio-container">
-              <input className="form-check-input" type="radio" value="PayPal" />
-              <label className="form-check-label">PayPal or Credit Card</label>
+    const cart = useSelector((state) => state.cart);
+    const { shippingAddress } = cart;
+
+    if (!shippingAddress) {
+        history.push("/shipping")
+    }
+    //todo walidacja typu platnosci
+    const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+    const dispatch = useDispatch();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(savePaymentMethod(paymentMethod));
+        history.push("/placeorder");
+    };
+
+    return (
+        <>
+            <Header />
+            <div className="container d-flex justify-content-center align-items-center login-center">
+                <form
+                    className="Login2 col-md-8 col-lg-4 col-11"
+                    onSubmit={submitHandler}
+                >
+                    <h6>SELECT PAYMENT METHOD</h6>
+                    <div className="payment-container">
+                        <div className="radio-container">
+                            <input className="form-check-input" checked type="radio" value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)} />
+                            <label className="form-check-label">PayPal or Credit Card</label>
+                        </div>
+                    </div>
+
+                    <button type="submit">Continue</button>
+                </form>
             </div>
-          </div>
-
-          <button type="submit">
-            <Link to="/placeorder" className="text-white">
-              Continue
-            </Link>
-          </button>
-        </form>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default PaymentScreen;
