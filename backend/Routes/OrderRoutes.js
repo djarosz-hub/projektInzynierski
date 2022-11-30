75c69be7cda9b08277812b2ff3f879be914c04f5
djarosz-hub <jaroszdamian9@gmail.com>
2022-11-30 22:12:38 +0100

    order datail api

--- backend/Routes/OrderRoutes.js
+++ backend/Routes/OrderRoutes.js
@@ -6,6 +6,7 @@ import Order from './../Models/OrderModel.js';
 const orderRoute = express.Router();
 
 orderRoute.post("/", protect, asyncHandler(async (req, res) => {
+
     const { orderItems, shippingAddress, paymentMethod, itemsPrice, shippingPrice, totalPrice } = req.body;
 
     if (orderItems && orderItems.length === 0) {
@@ -19,6 +20,21 @@ orderRoute.post("/", protect, asyncHandler(async (req, res) => {
 
         const createOrder = await order.save();
         res.status(201).json(createOrder)
+    }
+}));
+
+orderRoute.get("/:id", protect, asyncHandler(async (req, res) => {
+
+    const order = await Order.findById(req.params.id).populate(
+        "user",
+        "name email"
+    );
+
+    if (order) {
+        res.json(order);
+    } else {
+        res.status(404);
+        throw new Error("Order not found.");
     }
 }));
 
