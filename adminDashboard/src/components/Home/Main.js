242691dd592a3c52cb4fa1bb615d8353f974518d
djarosz-hub <jaroszdamian9@gmail.com>
2023-01-01 21:08:30 +0100

    mainpage statictics

--- adminDashboard/src/components/Home/Main.js
+++ adminDashboard/src/components/Home/Main.js
@@ -3,30 +3,38 @@ import TopTotal from "./TopTotal";
 import LatestOrder from "./LatestOrder";
 import SaleStatistics from "./SalesStatistics";
 import ProductsStatistics from "./ProductsStatistics";
+import { useSelector } from 'react-redux';
 
 const Main = () => {
-  return (
-    <>
-      <section className="content-main">
-        <div className="content-header">
-          <h2 className="content-title"> Dashboard </h2>
-        </div>
-        {/* Top Total */}
-        <TopTotal />
 
-        <div className="row">
-          {/* STATICS */}
-          <SaleStatistics />
-          <ProductsStatistics />
-        </div>
+    const orderList = useSelector((state) => state.orderList);
+    const { loading, error, orders } = orderList;
 
-        {/* LATEST ORDER */}
-        <div className="card mb-4 shadow-sm">
-          <LatestOrder />
-        </div>
-      </section>
-    </>
-  );
+    const productList = useSelector((state) => state.productList);
+    const { products } = productList;
+
+    return (
+        <>
+            <section className="content-main">
+                <div className="content-header">
+                    <h2 className="content-title"> Dashboard </h2>
+                </div>
+                {/* Top Total */}
+                <TopTotal orders={orders} products={products} />
+
+                <div className="row">
+                    {/* STATICS */}
+                    <SaleStatistics />
+                    <ProductsStatistics />
+                </div>
+
+                {/* LATEST ORDER */}
+                <div className="card mb-4 shadow-sm">
+                    <LatestOrder orders={orders} loading={loading} error={error} />
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default Main;
--- adminDashboard/src/components/Home/ProductsStatistics.js
+++ adminDashboard/src/components/Home/ProductsStatistics.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 const ProductsStatistics = () => {
-  return (
-    <div className="col-xl-6 col-lg-12">
-      <div className="card mb-4 shadow-sm">
-        <article className="card-body">
-          <h5 className="card-title">Products statistics</h5>
-          <img
-            style={{ width: "100%", height: "350px", objectFit: "contain" }}
-            src="/images/product.png"
-          />
-        </article>
-      </div>
-    </div>
-  );
+    return (
+        <div className="col-xl-6 col-lg-12">
+            <div className="card mb-4 shadow-sm">
+                <article className="card-body">
+                    <h5 className="card-title">Products statistics</h5>
+                    <iframe
+                        title="productsStatistics"
+                        style={{
+                            background: "#FFFFFF",
+                            border: "none",
+                            borderRadius: "2px",
+                            boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)",
+                            width: "100%",
+                            height: "350px"
+                        }}
+                        src="https://charts.mongodb.com/charts-ecommercedb-drnml/embed/charts?id=63b1dbf1-94f5-4890-86e2-a1fc56387c3c&maxDataAge=300&theme=light&autoRefresh=true"></iframe>
+                </article>
+            </div>
+        </div >
+    );
 };
 
 export default ProductsStatistics;
